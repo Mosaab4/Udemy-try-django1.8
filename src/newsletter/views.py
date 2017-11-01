c2ce69761f56cbdc11560e08c609d9e1a842471d
Mosaab Ehab <musab.ehab@gmail.com>
2017-11-01 22:15:49 +0200

    add some styling to the project

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -31,6 +31,8 @@ def home(request):
     return render(request,"home.html", context)
 
 def contact(request):
+    title= 'Contact Us' 
+    title_align_center = True  
     form = ContactForm(request.POST or None)
 
     if form.is_valid():
@@ -56,6 +58,8 @@ def contact(request):
         )
     context = {
         "form":form,
+        "title": title,
+        "title_align_center":title_align_center,
     }
 
     return render(request, "forms.html", context)
